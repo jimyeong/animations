980c78149e7344ed58a83a4d468949d3085f2abd
JIMMY JUNG <idjjm92@gmail.com>
2022-10-26 23:50:54 +0100

    matrix

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,53 +1,25 @@
-$(document).ready(function(){
-    app.init();
-    
-    
-})
+$(document).ready(function () {
+  app.init();
+});
 /* 
 const variables = {
     host: ""
 }; */
 
-const targetID ="박광현"
 const app = {
-    init:function(){
-        app.onClickSubmit();
-        app.onFocusOut();
-    },
-    getUserName: function(){
-        const username =  $('#username_ctrl').val();
-        return username;
-    },
-    onFocusOut: function(){
-        // 이렇게 하면 typing 될때마다, 변경을 탐지할 수 있다.
-        $(document).on('input', function(){
-            const button = $("#lucky_number-btn");
-            const target = $('#username_ctrl');
-            const value =  target.val();
-            if(value===targetID){button.removeClass('dn')};
-            if(value!==targetID){button.addClass('dn')};
-        })
-    },
-    onClickSubmit: function(){
-        // 행운번호 추첨 버튼
-        $('#lucky_number-btn').on('click', function(e){
-            e.stopPropagation();
-            $.fn.utils.navigate("/fortune_number.html");
-        });
-        $('#send_user_data').on('click', function(e){
-            e.stopPropagation();
-            
-            const username = app.getUserName(); // 데이터 가져오는 함수
-            const endPoint = SERVER_HOST;
-            const method = "POST";
-            const requestBody = {username};
-            location.href = `/processing.html?username=${username}`
-            const successCallback = ()=>{
-                // page전환
-                
-            }
-            const errorCallback = ()=>{}
-            // $.fn.utils.edenAjax(endPoint, method, successCallback, errorCallback, requestBody);
-        });
+  characters: "ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝ",
+  charactersArray: [],
+  init: function () {
+    for (let i = 0; i < app.characters.length - 20; i++) {
+      const nthChar = app.characters.charAt(i);
+      app.charactersArray.push(app.characters.charAt(nthChar));
+      $(".rain").append(`<span class="cube">${nthChar}</span>`);
     }
+    console.log("[characters]", app.charactersArray);
+    console.log("[length]: ", app.characters.length);
+    console.log(app.getRandomNumber(app.characters.length));
+  },
+  getRandomNumber: function (limit) {
+    return Math.floor(Math.random() * limit);
+  },
 };
